573c9d5ca0b613537dd379d32c92576624d6592a
usuario <usuario@KRDAIO5076>
2019-12-16 09:09:16 -0500

    Clase

--- ANDREA/src/Correr.java
+++ ANDREA/src/Correr.java
@@ -4,18 +4,36 @@ public class Correr {
 	public static void main (String args[]) {
 		Scanner sc = new Scanner (System.in);
 		
-		int[] vector= {3,4,0,0,9,-5,2,0,8};
-		int acumulador =0;
+		int[] vector= {3,4,0,0,9,-5,2,7,8};
+		int repeticiones=1;
 		
+		do {
+			repeticiones--;
 		for(int i=0; i<vector.length; i++) {
 			if(vector[i]==0) {
-				vector[i]=acumulador;
 				for(int j=i; j<vector.length-1; j++) {
+					if(vector[j+1]==0 && (j+1)!=vector.length-1) {
+						repeticiones++;
+						System.out.println("n"+repeticiones);
+					}
 					vector[j]= vector[j+1];
+					
 				}
+				
 				vector[vector.length-1]=0;
+				
 			}
-			System.out.println(vector[i]);
+			System.out.println();
+			imprimirVector(vector);
+			System.out.println("n"+repeticiones);
+		}
+		System.out.println("n"+repeticiones);
+		}while(repeticiones!=0);
+		imprimirVector(vector);
+	}
+	public static void imprimirVector(int arreglo []) {
+		for(int i=0; i<arreglo.length; i++) {
+			System.out.println(arreglo[i]);
 		}
 	}
 }
--- ANDREA/src/Vector.java
+++ ANDREA/src/Vector.java
@@ -31,3 +31,5 @@ public class Vector {
 	}
 
 }
+
+//Poner los ceros en la derecha
